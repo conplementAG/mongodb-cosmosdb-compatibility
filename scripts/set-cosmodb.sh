f2fab9255abc3446a3efa67e01e45e03c8d248a0
Gramoz Krasniqi <Gramoz.Krasniqi@conplement.de>
2019-09-30 11:30:51 +0200

    EnableAggregationPipeline preview feature

--- scripts/set-cosmodb.sh
+++ scripts/set-cosmodb.sh
@@ -7,7 +7,7 @@ db_name=test
 
 az group create -l $location -n $rg_name
 
-az cosmosdb create -n $cosmodb_account -g $rg_name --kind MongoDB
+az cosmosdb create -n $cosmodb_account -g $rg_name --kind MongoDB --capabilities "EnableAggregationPipeline"
 
 az cosmosdb mongodb database create --account-name $cosmodb_account --name $db_name --resource-group $rg_name
 
